ef0e43c117e9f2118dca3e3468ddbe64b62f41e2
littledot <littledot5566@gmail.com>
2017-04-18 11:08:46 -0700

    Ignore Go source created by Mockhiato.

--- cmd/gen.go
+++ cmd/gen.go
@@ -15,8 +15,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"gitlab.com/littledot/mockhiato/lib"
 )
@@ -33,7 +31,6 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("gen called")
 		lib.Run()
 	},
 }
