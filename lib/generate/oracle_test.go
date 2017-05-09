3f3dcc440b046bc38f0c030a380f05381629ce5c
Sheng-Dean Chang <sdchang@uber.com>
2017-05-08 17:39:14 -0700

    Move tests alongside source so that coverage can be calculated.

---
+++ lib/generate/oracle_test.go
@@ -0,0 +1,45 @@
+package generate_test
+
+import (
+	"io/ioutil"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/littledot/mockhiato/lib"
+	"github.com/littledot/mockhiato/lib/generate"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerate(t *testing.T) {
+	a := assert.New(t)
+
+	testPath := "test"
+	files, err := ioutil.ReadDir(testPath)
+	if err != nil {
+		a.FailNow(err.Error())
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			continue
+		}
+
+		t.Run(file.Name(), func(t *testing.T) {
+			actualPath := filepath.Join(testPath, file.Name(), "actual")
+
+			config := lib.Config{}
+			config.ProjectPath = actualPath
+			config.MockFileName = "mockhiato_mocks.go"
+
+			generate.Run(config)
+
+			expectPath := filepath.Join(testPath, file.Name(), "expect")
+			cmd := exec.Command("diff", "-r", expectPath, actualPath)
+			if stdout, err := cmd.CombinedOutput(); err != nil {
+				a.Error(err)
+				a.Fail(string(stdout))
+			}
+		})
+	}
+}
--- lib/generate/test/same_import_name/expect/demo.go
+++ lib/generate/test/same_import_name/expect/demo.go
@@ -1,8 +1,8 @@
 package example
 
 import (
-	"github.com/littledot/mockhiato/test/same_import_name/expect/same"
-	other_same "github.com/littledot/mockhiato/test/same_import_name/expect/same1"
+	"github.com/littledot/mockhiato/lib/generate/test/same_import_name/expect/same"
+	other_same "github.com/littledot/mockhiato/lib/generate/test/same_import_name/expect/same1"
 )
 
 // Target interface references 2 packages from different paths but with the same name. Mockhiato must be able to handle imports correctly for these kind of packages.
