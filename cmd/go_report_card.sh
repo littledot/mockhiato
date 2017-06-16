64ece0987780cbbdbe879b4b4d2e91163d0eaad0
Sheng-Dean Chang <sdchang@uber.com>
2017-06-16 01:58:59 -0700

    Refactor travis setup.

--- cmd/go_report_card.sh
+++ cmd/go_report_card.sh
@@ -1,1 +1,5 @@
-curl 'https://goreportcard.com/checks' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Referer: https://goreportcard.com/report/github.com/littledot/mockhiato' -H 'Connection: keep-alive' --data 'repo=github.com%2Flittledot%2Fmockhiato'
+#!/bin/bash
+set -ev
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  curl 'https://goreportcard.com/checks' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Referer: https://goreportcard.com/report/github.com/littledot/mockhiato' -H 'Connection: keep-alive' --data 'repo=github.com%2Flittledot%2Fmockhiato'
+fi
